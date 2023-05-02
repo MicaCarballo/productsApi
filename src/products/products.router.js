10e7fc7f174eb0d838edbc60994852815703d758
MicaCarballo <micacarballo2@gmail.com>
2023-05-02 20:24:33 -0300

    deleted unused imports

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -2,8 +2,7 @@ const router = require('express').Router()
 
 const productsServices = require('./products.services')
 const passportJWT = require('../middlewares/auth.middleware')
-const roleMiddleware= require('../middlewares/role.middleware')
-const { use } = require('chai')
+
 
 router.route('/')
     .get(productsServices.getAllProducts)
